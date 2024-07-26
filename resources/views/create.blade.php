17cbe7881889a39ed968cc6893286d44c805049b
yourboyha <w.wimonput@gmail.com>
2024-07-27 05:53:29 +0700

    25.FormsAndCSRFProtection

---
+++ resources/views/create.blade.php
@@ -0,0 +1,30 @@
+@extends('layouts.app')
+
+@section('title', 'Add Task')
+
+@section('content')
+  <form method="POST" action="{{route('tasks.store')}}">
+    @csrf
+    <div>
+      <label for="title">
+        Title
+      </label>
+      <input type="text" name="title" id="title">
+    </div>
+    <div>
+      <label>Description</label>
+      <textarea name="description" id="description" rows="5"></textarea>
+    </div>
+    <div>
+      <label for="long_description">Description</label>
+      <textarea name="long_description" id="long_description" rows="10"></textarea>
+    </div>
+
+    <div>
+      <button type="submit">Add task</button>
+    </div>
+
+  </form>
+    
+@endsection
+
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Http\Response;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 // class Task
@@ -68,11 +69,18 @@ Route::get('/tasks', function () {
     ]);
 })->name('tasks.index');
 
+Route::view('/tasks/create', 'create')
+    ->name('tasks.create');
+
 Route::get('/tasks/{id}', function($id) {
     return view('show',[
         'task' => \App\Models\Task::findOrFail($id)
     ]);
 })->name('tasks.show');
+
+Route::post('\tasks', function(Request $request){
+    dd($request->all());
+})->name('tasks.store');
 
 // Route::get('/tasks/{id}', function($id) use ($tasks) {
 //     $task = collect($tasks) -> firstWhere('id', $id);
