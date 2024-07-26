9210d419fe87b0688a3e65d2d01731c0ae3c1789
yourboyha <w.wimonput@gmail.com>
2024-07-27 05:14:06 +0700

    AddProjectTasklist

---
+++ resources/views/index.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.app')
+
+@section('title', 'The list of tasks')
+
+
+@section('content')
+    {{-- @if(count($tasks)) --}}
+    @forelse ($tasks as $task)
+        <div>
+            <a href="{{ route('tasks.show',['id'=>$task->id])}}">{{$task->title}}</a> 
+        </div>
+    @empty
+        <div>There are no tasks!</div>
+    @endforelse
+    {{-- @endif --}}
+@endsection
---
+++ routes/web.php
@@ -0,0 +1,102 @@
+<?php
+
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Route;
+
+// class Task
+// {
+//     public function __construct(
+//         public int $id,
+//         public string $title,
+//         public string $description,
+//         public ?string $long_description,
+//         public bool $completed,
+//         public string $created_at,
+//         public string $updated_at
+//     ) {
+//     }
+// }
+
+// $tasks = [
+//     new Task(
+//         1,
+//         'Buy groceries',
+//         'Task 1 description',
+//         'Task 1 long description',
+//         false,
+//         '2023-03-01 12:00:00',
+//         '2023-03-01 12:00:00'
+//     ),
+//     new Task(
+//         2,
+//         'Sell old stuff',
+//         'Task 2 description',
+//         null,
+//         false,
+//         '2023-03-02 12:00:00',
+//         '2023-03-02 12:00:00'
+//     ),
+//     new Task(
+//         3,
+//         'Learn programming',
+//         'Task 3 description',
+//         'Task 3 long description',
+//         true,
+//         '2023-03-03 12:00:00',
+//         '2023-03-03 12:00:00'
+//     ),
+//     new Task(
+//         4,
+//         'Take dogs for a walk',
+//         'Task 4 description',
+//         null,
+//         false,
+//         '2023-03-04 12:00:00',
+//         '2023-03-04 12:00:00'
+//     ),
+// ];
+
+Route::get('/', function () {
+    return redirect()->route('tasks.index');
+});
+
+Route::get('/tasks', function () {
+    return view('index',[
+        'tasks' => \App\Models\Task::all()
+        // $tasks
+    ]);
+})->name('tasks.index');
+
+Route::get('/tasks/{id}', function($id) {
+    return view('show',[
+        'task' => \App\Models\Task::findOrFail($id)
+    ]);
+})->name('tasks.show');
+
+// Route::get('/tasks/{id}', function($id) use ($tasks) {
+//     $task = collect($tasks) -> firstWhere('id', $id);
+
+//     if (!$task) {
+//         abort(Response::HTTP_NOT_FOUND);
+//     }
+
+//     return view('show',['task' => $task]);
+// })->name('tasks.show');
+
+
+// Route::get('/xxx', function () {
+//     return 'hello';
+// })->name('hello');
+
+// Route::get('/hallo', function () {
+//     return redirect()->route('hello');
+// });
+
+// Route::get('/greet/{name}', function ($name) {
+//     return "Hello, $name!";
+// });
+
+Route::fallback(function () {
+    return 'Still got Somewhere';
+});
+
